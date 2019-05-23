4efea6d738c8e127c321069c8641897d489c5c45
Jose7198 <jose719810@hotmail.com>
2019-05-23 16:55:21 -0500

    Parcelable
    Intercambio de objetos y datos mediante Parcelable (Serializacion)

---
+++ NewApp0523/app/src/main/java/com/example/newapp0523/MainActivity.kt
@@ -0,0 +1,36 @@
+package com.example.newapp0523
+
+import android.content.Intent
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        btn_parcelable.setOnClickListener {
+            irAParcelable()
+        }
+
+    }
+
+    fun irAParcelable(){
+        val intentExplicito = Intent(
+            this, Parcelable::class.java
+        )
+
+        val jose = Usuario("Jose", 21, Date(), 10.0)
+        intentExplicito.putExtra("usuario", jose)
+
+        val sherlock = Mascota("Sherlock", jose)
+        intentExplicito.putExtra("mascota", sherlock)
+
+        startActivity(intentExplicito)
+
+    }
+
+}
